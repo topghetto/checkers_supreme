09bf6ffa7bf9281706169a7de87fb23dbf58801a
Farouk Ojeleye <faroukoje@live.co.uk>
2015-02-23 01:33:51 +0000

    Linked the multiplayer button in the main menu to the checkers activity :)

--- Source Code/Checkers/src/uk/ac/kcl/www/MainActivity.java
+++ Source Code/Checkers/src/uk/ac/kcl/www/MainActivity.java
@@ -16,22 +16,13 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-				
-				//Button singlePlayerBtn = (Button) findViewById(R.id.single_player_btn);
-				
-				
-				// Whenever I would try to implement a GridView, the application would just crash on launch, and I believe
-				// it was because of this intent.
-				/*intent = new Intent(this, CheckersGame.class);
-				
-				singlePlayerBtn.setOnClickListener(new View.OnClickListener(){
-						public void onClick(View v){
-								
-								// It shall start the new activity, which is the 'single player' game
-								startActivity(intent);
-						}
-				});*/
-				
+        setContentView(R.layout.main);							
     }
+		public void startMultiplayer(View view)
+		{
+				// Prepares the Intent for the Multiplayer Activity.
+				Intent intent = new Intent(this, CheckersGame.class);
+				// Load the Multiplayer Activity.
+				startActivity(intent);
+		}
 }
